1cca0bd95e5aba1a351c1a4eb863680e74dfd977
ShaiDahan-code <shaidahan97@gmail.com>
2023-02-02 22:00:24 +0200

    Setup the game

--- src/app/app-main-page/sudoku/sudoku.component.ts
+++ src/app/app-main-page/sudoku/sudoku.component.ts
@@ -1,5 +1,10 @@
 import {Component, Input} from '@angular/core';
 
+export interface GridPosition {
+  row:number;
+  col:number;
+}
+
 @Component({
   selector: 'app-sudoku',
   templateUrl: './sudoku.component.html',
@@ -7,7 +12,7 @@ import {Component, Input} from '@angular/core';
 })
 export class SudokuComponent {
   grid!: string[][];
-  selectedCell: string = '';
+  selectedCell: GridPosition = {row:-1, col:-1};
 
   @Input()
   set sudokuString(sudoku: string) {
@@ -21,4 +26,33 @@ export class SudokuComponent {
   }
 
 
+  selectCell(row: number, col: number) {
+    this.selectedCell.row = row;
+    this.selectedCell.col = col;
+    console.log(this.grid[row][col]);
+
+  }
+
+  selectedNumber(numberSelected : number) {
+    this.grid[this.selectedCell.row][this.selectedCell.col] = numberSelected.toString();
+    this.selectedCell.row = -1;
+    this.selectedCell.col = -1;
+  }
+
+  displayOptions() {
+    return this.selectedCell.row !== -1 && this.selectedCell.col !== -1
+  }
+
+  arrayToDisplay() {
+    let arr = [1,2,3,4,5,6,7,8,9];
+    if(this.grid[this.selectedCell.row][this.selectedCell.col] === '0') {
+      return arr;
+    }
+    //remove from the arr the number in this.grid[this.selectedCell.row][this.selectedCell.col]
+    arr.splice(arr.indexOf(parseInt(this.grid[this.selectedCell.row][this.selectedCell.col])),1);
+    return arr;
+
+
+
+  }
 }
