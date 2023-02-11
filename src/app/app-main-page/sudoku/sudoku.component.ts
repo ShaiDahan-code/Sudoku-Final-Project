bc8eae3a9e5980f98d950d1027616b278eebe5c0
salush <sarel190@gmail.com>
2023-02-11 03:10:43 +0200

    Added frame to all cell that come from the soduko file

--- src/app/app-main-page/sudoku/sudoku.component.ts
+++ src/app/app-main-page/sudoku/sudoku.component.ts
@@ -20,19 +20,19 @@ export interface Cell{
 })
 export class SudokuComponent {
   grid!: Cell[][];
-  selectedCell: GridPosition = {row:-1, col:-1};
+  selectedCell: GridPosition = {row: -1, col: -1};
   unvalidBoard: boolean = false;
 
   invalidCell: boolean = false;
 
-  @Input() sudokuStringSolve !:string;
+  @Input() sudokuStringSolve !: string;
+
   @Input()
   set sudokuString(sudoku: string) {
-    if(!sudoku){
+    if (!sudoku) {
       this.unvalidBoard = true;
       return;
-    }
-    else if(sudoku.length !== 81) {
+    } else if (sudoku.length !== 81) {
       this.unvalidBoard = true;
       return;
     }
@@ -40,55 +40,60 @@ export class SudokuComponent {
     for (let i = 0; i < 9; i++) {
       this.grid[i] = [];
       for (let j = 0; j < 9; j++) {
-        this.grid[i][j] = {content: sudoku[i*9+j], editable: false, style:new Set([]), isBlock:false}
+        this.grid[i][j] = {content: sudoku[i * 9 + j], editable: false, style: new Set([]), isBlock: false}
         this.grid[i][j].editable = true ? this.grid[i][j].content == "0" : false;
+        if(!this.grid[i][j].editable){
+          this.grid[i][j].style.add("filled");
+        }
       }
     }
   }
 
 
   selectCell(row: number, col: number) {
-    if(!this.grid[row][col].editable || this.grid[row][col].isBlock) {
+    if (!this.grid[row][col].editable || this.grid[row][col].isBlock) {
       this.selectedCell.row = -1;
       this.selectedCell.col = -1;
       return;
     }
 
     this.deleteSelectedCell();
+    this.displaySelectedValid(row,col);
     this.grid[row][col].style.add("selected");
     this.selectedCell.row = row;
     this.selectedCell.col = col;
   }
+
   deleteSelectedCell() {
-    for (let i = 0 ; i <9 ; i++){
-      for(let j =0 ; j < 9 ; j++){
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
         this.grid[i][j].style.delete("selected");
+        this.grid[i][j].style.delete("hover");
       }
     }
   }
-  selectedNumber(numberSelected : number) {
+
+  selectedNumber(numberSelected: number) {
     this.grid[this.selectedCell.row][this.selectedCell.col].content = numberSelected.toString();
-    this.checkForValidation(this.selectedCell.row,this.selectedCell.col,numberSelected);
+    this.checkForValidation(this.selectedCell.row, this.selectedCell.col, numberSelected);
     console.log(this.grid);
     this.selectedCell.row = -1;
     this.selectedCell.col = -1;
   }
 
-  checkForValidation(row: number, col: number, numberSelected : number){
+  checkForValidation(row: number, col: number, numberSelected: number) {
     let is_invalid = false;
-    for(let i = 0 ; i< 9 ;i++){
-      if (numberSelected != 0 && this.grid[row][i].content == numberSelected.toString() && i != col){
+    for (let i = 0; i < 9; i++) {
+      if (numberSelected != 0 && this.grid[row][i].content == numberSelected.toString() && i != col) {
         this.grid[row][i].style.add("err");
         is_invalid = true;
-      }
-      else{
+      } else {
         this.grid[row][i].style.delete("err");
       }
-      if (numberSelected != 0 && this.grid[i][col].content == numberSelected.toString() && i != row){
+      if (numberSelected != 0 && this.grid[i][col].content == numberSelected.toString() && i != row) {
         this.grid[i][col].style.add("err");
         is_invalid = true;
-      }
-      else{
+      } else {
         this.grid[i][col].style.delete("err");
       }
     }
@@ -103,78 +108,104 @@ export class SudokuComponent {
     if (col % 3 == 2)
       centerCol -= 1;
 
-    for(let i = centerRow-1 ; i < centerRow+2 ;i++){
-      for(let j = centerCol-1 ; j < centerCol+2 ;j++){
-        if(row != i || col != j){
-          if(numberSelected && numberSelected.toString() == this.grid[i][j].content){
+    for (let i = centerRow - 1; i < centerRow + 2; i++) {
+      for (let j = centerCol - 1; j < centerCol + 2; j++) {
+        if (row != i || col != j) {
+          if (numberSelected && numberSelected.toString() == this.grid[i][j].content) {
             this.grid[i][j].style.add("err");
             is_invalid = true;
-          }
-          else{
+          } else {
             this.grid[i][j].style.delete("err");
           }
         }
       }
     }
 
-    if(is_invalid) {
+    if (is_invalid) {
       this.grid[row][col].style.add("invalid");
       this.grid[row][col].style.delete("valid");
-      this.BlockAllButtons(row,col)
-    }
-    else{
-      this.ReleaseAllButtons(row,col)
+      this.BlockAllButtons(row, col)
+    } else {
+      this.ReleaseAllButtons(row, col)
       this.grid[row][col].style.add("valid");
       this.grid[row][col].style.delete("invalid");
     }
 
 
   }
+
   BlockAllButtons(row: number, col: number) {
-    for (let i = 0 ; i <9 ; i++){
-      for(let j =0 ; j < 9 ; j++){
-        if(i != row || j != col){
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (i != row || j != col) {
           this.grid[i][j].isBlock = true;
         }
       }
     }
   }
+
   ReleaseAllButtons(row: number, col: number) {
-    for (let i = 0 ; i <9 ; i++){
-      for(let j =0 ; j < 9 ; j++){
-        if(i != row || j != col){
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (i != row || j != col) {
           this.grid[i][j].isBlock = false;
         }
       }
     }
   }
+
   displayOptions() {
     return this.selectedCell.row !== -1 && this.selectedCell.col !== -1
   }
 
   arrayToDisplay() {
-    let arr = [0,1,2,3,4,5,6,7,8,9];
+    let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     //remove from the arr the number in this.grid[this.selectedCell.row][this.selectedCell.col]
-    arr.splice(arr.indexOf(parseInt(this.grid[this.selectedCell.row][this.selectedCell.col].content)),1);
+    arr.splice(arr.indexOf(parseInt(this.grid[this.selectedCell.row][this.selectedCell.col].content)), 1);
     return arr;
 
   }
 
   getClasses(style: Set<string>) {
-    let classes :string = "";
+    let classes: string = "";
     for (let st of style) {
-      classes += st +" ";
+      classes += st + " ";
     }
     return classes;
   }
 
   addStyle(i: number, j: number) {
-    let style ="";
+    let style = "";
     if (i === 2 || i === 5)
       style += " margin-right: 10px;";
     if (j === 2 || j === 5)
       style += " margin-bottom: 10px;";
     return style;
+  }
+
+  private displaySelectedValid(row: number, col: number) {
+    for (let i = 0; i < 9; i++) {
+      this.grid[row][i].style.add("hover");
+      this.grid[i][col].style.add("hover");
+    }
+
+    let centerRow = row;
+    let centerCol = col;
+    if (row % 3 == 0)
+      centerRow += 1;
+    if (row % 3 == 2)
+      centerRow -= 1;
+    if (col % 3 == 0)
+      centerCol += 1;
+    if (col % 3 == 2)
+      centerCol -= 1;
+
+    for (let i = centerRow - 1; i < centerRow + 2; i++) {
+      for (let j = centerCol - 1; j < centerCol + 2; j++) {
+        this.grid[i][j].style.add("hover");
+
+      }
+    }
   }
 }
 
